f34afc1bdd9d11e8d5d443d08cee8addc5752e6d
Alechilles <22alec22@gmail.com>
2026-02-06 17:29:49 -0500

    Added commands for checking tamed status

---
+++ src/main/java/com/alechilles/alecstamework/commands/TameworkGetTamedCommand.java
@@ -0,0 +1,48 @@
+package com.alechilles.alecstamework.commands;
+
+import com.alechilles.alecstamework.npc.components.TameworkTamedComponent;
+import com.hypixel.hytale.component.ComponentType;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import javax.annotation.Nonnull;
+
+/**
+ * Command to display tamed status for the targeted NPC.
+ */
+public final class TameworkGetTamedCommand extends AbstractPlayerCommand {
+
+    public TameworkGetTamedCommand() {
+        super("gettamed", "Get tamed status of the NPC you are looking at.");
+        setAllowsExtraArguments(true);
+    }
+
+    @Override
+    protected void execute(@Nonnull CommandContext commandContext,
+                           @Nonnull Store<EntityStore> store,
+                           @Nonnull Ref<EntityStore> ref,
+                           @Nonnull PlayerRef playerRef,
+                           @Nonnull World world) {
+        TameworkCommandTargeting.Candidate candidate = TameworkCommandTargeting.findTargetNpc(store, ref);
+        if (candidate == null || candidate.ref == null || !candidate.ref.isValid()) {
+            commandContext.sender().sendMessage(Message.raw("No NPC found in view."));
+            return;
+        }
+
+        boolean tamed = false;
+        ComponentType<EntityStore, TameworkTamedComponent> type = TameworkTamedComponent.getComponentType();
+        if (type != null) {
+            TameworkTamedComponent component = store.getComponent(candidate.ref, type);
+            if (component != null) {
+                tamed = component.isTamed();
+            }
+        }
+
+        commandContext.sender().sendMessage(Message.raw("Tamed for NPC " + candidate.npcUuid + " is " + tamed));
+    }
+}
