e62959c89cf5b22c3ef1b158588b13a9dcd411ce
Alechilles <22alec22@gmail.com>
2026-02-18 15:19:41 -0500

    Fix: expose hook target position via info provider
    Tests: ./mvnw test (pass)

--- src/main/java/com/alechilles/alecstamework/npc/sensorinfo/TameworkHookInfoProvider.java
+++ src/main/java/com/alechilles/alecstamework/npc/sensorinfo/TameworkHookInfoProvider.java
@@ -2,6 +2,7 @@ package com.alechilles.alecstamework.npc.sensorinfo;
 
 import com.hypixel.hytale.server.npc.sensorinfo.InfoProviderBase;
 import com.hypixel.hytale.server.npc.sensorinfo.IPositionProvider;
+import com.hypixel.hytale.server.npc.sensorinfo.PositionProvider;
 import com.hypixel.hytale.server.npc.sensorinfo.parameterproviders.ParameterProvider;
 import javax.annotation.Nullable;
 
@@ -9,17 +10,32 @@ import javax.annotation.Nullable;
  * Info provider for Tamework hook sensors.
  */
 public final class TameworkHookInfoProvider extends InfoProviderBase {
+    private final TameworkHookInfo hookInfo;
+    private final PositionProvider positionProvider;
+
     public TameworkHookInfoProvider(@Nullable ParameterProvider parameterProvider, TameworkHookInfo hookInfo) {
         super(parameterProvider, hookInfo);
+        this.hookInfo = hookInfo;
+        this.positionProvider = new PositionProvider(parameterProvider, hookInfo);
     }
 
     @Override
     public IPositionProvider getPositionProvider() {
-        return null;
+        refreshPositionProvider();
+        return positionProvider;
     }
 
     @Override
     public boolean hasPosition() {
-        return false;
+        refreshPositionProvider();
+        return positionProvider.hasPosition();
+    }
+
+    private void refreshPositionProvider() {
+        if (hookInfo != null && hookInfo.hasTargetPosition()) {
+            positionProvider.setTarget(hookInfo.getTargetX(), hookInfo.getTargetY(), hookInfo.getTargetZ());
+        } else {
+            positionProvider.clear();
+        }
     }
 }
