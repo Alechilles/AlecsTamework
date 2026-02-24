cfa7b062d58ad1cae6f4e4bfd0c5ece65cebbdf4
Alechilles <22alec22@gmail.com>
2026-02-24 14:12:48 -0500

    Build: support hosted Java runner in publish workflow

--- mvnw.cmd
+++ mvnw.cmd
@@ -16,8 +16,33 @@ if not exist "%WRAPPER_JAR%" (
     exit /b 1
 )
 
-@rem Use bundled JDK from Hytale Toolkit
-set JAVACMD=C:\Program Files\Eclipse Adoptium\jdk-25.0.1.8-hotspot\bin\java.exe
+@rem Resolve Java in order:
+@rem 1) JAVA_HOME (CI and local)
+@rem 2) java.exe on PATH
+@rem 3) Local fallback JDK path used by the Hytale toolkit
+set "JAVACMD="
+
+if not "%JAVA_HOME%"=="" (
+    if exist "%JAVA_HOME%\bin\java.exe" (
+        set "JAVACMD=%JAVA_HOME%\bin\java.exe"
+    )
+)
+
+if "%JAVACMD%"=="" (
+    for %%I in (java.exe) do set "JAVACMD=%%~$PATH:I"
+)
+
+if "%JAVACMD%"=="" (
+    set "LOCAL_JAVA=C:\Program Files\Eclipse Adoptium\jdk-25.0.1.8-hotspot\bin\java.exe"
+    if exist "%LOCAL_JAVA%" (
+        set "JAVACMD=%LOCAL_JAVA%"
+    )
+)
+
+if "%JAVACMD%"=="" (
+    echo Error: Java runtime not found. Set JAVA_HOME or add java.exe to PATH.
+    exit /b 1
+)
 
 set MAVEN_PROJECTBASEDIR=%BASEDIR%
 
