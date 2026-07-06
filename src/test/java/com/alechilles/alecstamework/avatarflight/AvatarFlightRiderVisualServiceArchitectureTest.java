d669f1c75d21f1970bf1a5f1837d44398a63a513
Alechilles <22alec22@gmail.com>
2026-07-06 09:26:30 -0400

    Fix: keep avatar rider hands straight

--- src/test/java/com/alechilles/alecstamework/avatarflight/AvatarFlightRiderVisualServiceArchitectureTest.java
+++ src/test/java/com/alechilles/alecstamework/avatarflight/AvatarFlightRiderVisualServiceArchitectureTest.java
@@ -78,7 +78,6 @@ class AvatarFlightRiderVisualServiceArchitectureTest {
         assertTrue(json.contains("\"x\": 0.707107"));
         assertTrue(json.contains("\"x\": -0.461749"));
         assertTrue(json.contains("\"x\": -0.300706"));
-        assertTrue(json.contains("\"x\": 0.130526"));
         assertTrue(json.split("\"isPiece\": true", -1).length - 1 == 1);
     }
 
